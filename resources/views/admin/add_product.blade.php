80e108622d0b4a9d9436502ae3485a6a60ad535c
sobel <sobelkantinath@gmail.com>
2024-09-10 14:26:59 +0600

    Commit

--- resources/views/admin/add_product.blade.php
+++ resources/views/admin/add_product.blade.php
@@ -1,12 +1,80 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <h1>I am from add product</h1>
-</body>
+<html>
+  <head> 
+    @include('admin.css')
+    <style type="text/css">
+      .div_deg
+      {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 60px;
+      }
+      h1{
+        color: white;
+      }
+      label{
+        display: inline-block;
+        width: 250px;
+        font-size: 20px!important;
+        color: white!important;
+      }
+
+      input{
+        widows: 200px;
+        height: 30px;
+      }
+    </style>
+  </head>
+  <body>
+    
+    {{-- Start of header work --}}
+      @include('admin.header')
+    {{-- End of header work --}}
+      
+    {{-- Start of Sidebar Navigation Work --}}
+      @include('admin.sidebar')
+    {{-- End of Sidebar Navigation Work --}}
+
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            <h1>Add Product</h1>
+            <div class = div_deg>
+              <form action="">
+                <div>
+                  <label>Product Title</label>
+                  <input type="text" name="title" required>
+                </div>
+                <div>
+                  <label >Description</label>
+                  <textarea name="description" required></textarea>
+                </div>
+                <div>
+                  <label>Price</label>
+                  <input type="number" name="price" id="">
+                </div>
+                <div>
+                  <label >Quantity</label>
+                  <input type="number" name="quantity" id="">
+                </div>
+                <div>
+                  <label >Product Category</label>
+                  <select name="" id="">
+                    <option value="">abc</option>
+                  </select>
+                </div>
+                <div>
+                  <label >Product Image</label>
+                  <input type="file" name="image" id="">
+                </div>
+              </form>
+            </div>
+
+          </div>
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    @include('admin.js')
+  </body>
 </html>
